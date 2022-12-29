5ece4e2e94741e13d854c6e5490144fc9504cc8e
Giridharaprasath M <girisagayam@gmail.com>
2022-12-29 13:53:08 +0530

    Initial Build

---
+++ Source/ATLAProject/GameInstanceSubsystem/ATLAGameInstanceSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ATLAGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATLAPROJECT_API UATLAGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
