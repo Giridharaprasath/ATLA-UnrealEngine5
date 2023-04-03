1de85c406e5634b9359ac6e8b0cb3344e1f92a78
Giridharaprasath M <girisagayam@gmail.com>
2023-04-04 02:40:36 +0530

    Added Game Info Debug Messages

--- Source/ATLAProject/HUD/ATLAHUD.cpp
+++ Source/ATLAProject/HUD/ATLAHUD.cpp
@@ -2,3 +2,36 @@
 
 
 #include "ATLAHUD.h"
+
+FString AATLAHUD::GetGameName()
+{
+	FString GameName;
+	GConfig->GetString(
+		TEXT("/Script/EngineSettings.GeneralProjectSettings"),
+		TEXT("ProjectName"),
+		GameName,
+		GGameIni);
+	return GameName;
+}
+
+FString AATLAHUD::GetGameVersion()
+{
+	FString GameVersion;
+	GConfig->GetString(
+		TEXT("/Script/EngineSettings.GeneralProjectSettings"),
+		TEXT("ProjectVersion"),
+		GameVersion,
+		GGameIni);
+	return GameVersion;
+}
+
+FString AATLAHUD::GetCompanyName()
+{
+	FString CompanyName;
+	GConfig->GetString(
+		TEXT("/Script/EngineSettings.GeneralProjectSettings"),
+		TEXT("CompanyName"),
+		CompanyName,
+		GGameIni);
+	return CompanyName;
+}
--- Source/ATLAProject/HUD/ATLAHUD.h
+++ Source/ATLAProject/HUD/ATLAHUD.h
@@ -6,7 +6,6 @@
 #include "GameFramework/HUD.h"
 #include "ATLAHUD.generated.h"
 
-
 /**
  * ATLA HUD Class.
  */
@@ -32,4 +31,16 @@ public:
 	// Set Current Player Character Name
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "ATLA|HUD")
 	void SetCharacterName(const FText& Name);
+
+	// Get Game Name
+	UFUNCTION(BlueprintPure, Category = "ATLA|HUD|Debug")
+	static FString GetGameName();
+
+	// Get Game Version
+	UFUNCTION(BlueprintPure, Category = "ATLA||HUD|Debug")
+	static FString GetGameVersion();
+
+	// Get Company Name
+	UFUNCTION(BlueprintPure, Category = "ATLA|HUD|Debug")
+	static FString GetCompanyName();
 };
