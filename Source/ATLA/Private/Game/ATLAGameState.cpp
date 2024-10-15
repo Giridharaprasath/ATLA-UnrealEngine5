9413e406e18006cf000c6b50a40d9999ffe31451
Giridharaprasath M <girisagayam@gmail.com>
2024-10-15 11:31:15 +0530

    Other Players Info In Process

---
+++ Source/ATLA/Private/Game/ATLAGameState.cpp
@@ -0,0 +1,3 @@
+// Copyright Melon Studios.
+
+#include "Game/ATLAGameState.h"
---
+++ Source/ATLA/Public/Game/ATLAGameState.h
@@ -0,0 +1,18 @@
+// Copyright Melon Studios.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+
+#include "ATLAGameState.generated.h"
+
+/**
+ *	ATLA Game State Class.
+ */
+UCLASS()
+class ATLA_API AATLAGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
